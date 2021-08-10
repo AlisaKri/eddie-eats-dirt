863a666dd230f1ee833dd30c15fdf9360421bb42
Alisa Krishtal <alisa@Alisas-MBP.fios-router.home>
2021-08-10 18:12:33 -0400

    bug fix for image path in dirt.html

--- scripts/says.js
+++ scripts/says.js
@@ -19,36 +19,45 @@ var PLAYINPROGRESS = false;
 var CHORDS = ['E', 'A', 'C', 'G', 'D', 'F']
 
 var CHORD_INSTRUCTIONS = new Object();
-CHORD_INSTRUCTIONS['E'] = ['Place the middle finger on the SECOND FRET of the A STRING', 
-                           'Place the ring finger on the SECOND FRET of the D STRING', 
-                           'Place the index finger on the FIRST FRET of the G STRING'];
-CHORD_INSTRUCTIONS['A'] = ['Bar the SECOND FRET of the D STRING',
-                            'Bar the SECOND FRET of the G STRING',
-                            'Bar the SECOND FRET of the B STRING'];
-CHORD_INSTRUCTIONS['C'] = ['Place the ring finger on the THIRD FRET of the A STRING', 
-                           'Place the middle finger on the SECOND FRET of the D STRING', 
-                           'Place the index finger on the FIRST FRET of the B STRING'];
+CHORD_INSTRUCTIONS['E'] = ['Leave the LOW E string OPEN',
+                            'Place the middle finger on the SECOND FRET of the A string', 
+                           'Place the ring finger on the SECOND FRET of the D string', 
+                           'Place the index finger on the FIRST FRET of the G string',
+                            'Leave the B string OPEN',
+                            'Leave the HIGH E string OPEN'];
+CHORD_INSTRUCTIONS['A'] = ['Leave the A string OPEN',
+                            'Bar the SECOND FRET of the D string',
+                            'Bar the SECOND FRET of the G string',
+                            'Bar the SECOND FRET of the B string'];
+CHORD_INSTRUCTIONS['C'] = ['Place the ring finger on the THIRD FRET of the A string', 
+                           'Place the middle finger on the SECOND FRET of the D string', 
+                           'Leave the G string OPEN',
+                           'Place the index finger on the FIRST FRET of the B string',
+                           'Leave the HIGH E string OPEN',];
 
-CHORD_INSTRUCTIONS['G'] = ['Place the middle finger on the THIRD FRET of the LOW E STRING', 
-                           'Place the index finger on the SECOND FRET of the A STRING', 
-                           'Place the ring finger on the THIRD FRET of the B STRING',
-                            'Place the pinky on the THIRD FRET of the HIGH E STRING'];
+CHORD_INSTRUCTIONS['G'] = ['Place the middle finger on the THIRD FRET of the LOW E string', 
+                           'Place the index finger on the SECOND FRET of the A string', 
+                           'Leave the D string OPEN',
+                           'Leave the G string OPEN',
+                           'Place the ring finger on the THIRD FRET of the B string',
+                            'Place the pinky on the THIRD FRET of the HIGH E string'];
 
-CHORD_INSTRUCTIONS['D'] = ['Place the index finger on the SECOND FRET of the G STRING', 
-                            'Place the ring finger on the THIRD FRET of the B STRING', 
-                            'Place the middle finger on the SECOND FRET of the HIGH E STRING'];
+CHORD_INSTRUCTIONS['D'] = ['Leave the D string OPEN',
+                            'Place the index finger on the SECOND FRET of the G string', 
+                            'Place the ring finger on the THIRD FRET of the B string', 
+                            'Place the middle finger on the SECOND FRET of the HIGH E string'];
 
-CHORD_INSTRUCTIONS['F'] = ['Bar the FIRST FRET of the HIGH E STRING', 
-                            'Bar the FIRST FRET of the B STRING', 
-                            'Place the middle finger on the SECOND FRET of the G STRING',
-                            'Place the ring finger on the THIRD FRET of the D STRING'];
+CHORD_INSTRUCTIONS['F'] = ['Bar the FIRST FRET of the HIGH E string', 
+                            'Bar the FIRST FRET of the B string', 
+                            'Place the middle finger on the SECOND FRET of the G string',
+                            'Place the pinky finger on the THIRD FRET of the D string'];
 
 var CHORD_NOTES = new Object(); 
-CHORD_NOTES['E'] = [[4, 2], [3,2], [2,1]]; /*[stringNumber, fretNumber] counting from highE = 0 and open string = 0*/
-CHORD_NOTES['A'] = [[3, 2], [2, 2], [1, 2]];
-CHORD_NOTES['C'] = [[4, 3], [3, 2], [1, 1]];
-CHORD_NOTES['G'] = [[5, 3], [4, 2], [1, 3], [0, 3]];
-CHORD_NOTES['D'] = [[2,2],[1,3],[0,2]];
+CHORD_NOTES['E'] = [[5, 0], [4, 2], [3,2], [2,1], [1, 0], [0, 0]]; /*[stringNumber, fretNumber] counting from highE = 0 and open string = 0*/
+CHORD_NOTES['A'] = [[4, 0], [3, 2], [2, 2], [1, 2]];
+CHORD_NOTES['C'] = [[4, 3], [3, 2], [2, 0], [1, 1], [0, 0]];
+CHORD_NOTES['G'] = [[5, 3], [4, 2], [3, 0], [2, 0 ], [1, 3], [0, 3]];
+CHORD_NOTES['D'] = [[3, 0], [2, 2], [1, 3], [0, 2]];
 CHORD_NOTES['F'] = [[0, 1], [1, 1], [2, 2], [3, 3]];
 
 
